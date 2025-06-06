dfd01a21b0f3a6ba7391822521c73638596ebcab
Error404 <apupaul35@gmail.com>
2025-06-06 11:58:56 +0600

    Added Private route to secure the route

--- src/pages/BookShelf/BookCard.jsx
+++ src/pages/BookShelf/BookCard.jsx
@@ -6,20 +6,20 @@ const BookCard = ({ singleBook }) => {
     const {_id,book_title,cover_photo,book_author,book_category,upvote} = singleBook;
 
     return (
-        <div className="card card-side bg-base-100 shadow-sm">
-            <figure>
+        <div className="card card-side bg-slate-800 shadow-sm">
+            <figure className='w-1/2'>
                 <img
                     src={cover_photo}
                     alt="Movie" />
             </figure>
-            <div className="card-body">
-                <h2 className="card-title">{book_title}</h2>
-                <p>Written By - {book_author}</p>
+            <div className="card-body w-1/2">
+                <h2 className="card-title text-white">{book_title}</h2>
+                <p className='text-white'>Written By - {book_author}</p>
                 <button className=' badge badge-secondary'>{book_category}</button>
                 <div className="card-actions">
                     <button className="btn btn-primary">Upvote: {upvote}</button>
                 </div>
-                <div className='w-1/2 mx-auto mt-8'>
+                <div className='w-full text-center mt-8'>
                     <Link to={`/books/${_id}`}><button className='btn btn-neutral'>View Details</button></Link>
                 </div>
             </div>
--- src/pages/BookShelf/BookShelf.jsx
+++ src/pages/BookShelf/BookShelf.jsx
@@ -11,6 +11,23 @@ const BookShelf = () => {
     return (
         <div className='min-h-screen w-10/12 mx-auto my-12'>
             <h2 className='text-3xl font-extrabold text-center'>Browse All the Books</h2>
+            <div className='text-center w-10/12 mx-auto mt-8'>
+                            <label className="input w-full">
+                <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <g
+                        strokeLinejoin="round"
+                        strokeLinecap="round"
+                        strokeWidth="2.5"
+                        fill="none"
+                        stroke="currentColor"
+                    >
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <path d="m21 21-4.3-4.3"></path>
+                    </g>
+                </svg>
+                <input type="search" className="grow" placeholder="Search your books" />
+            </label>
+            </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-10'>
                 {
                     allBooks.map(singleBook => (<BookCard key={singleBook._id} singleBook={singleBook}></BookCard>))
